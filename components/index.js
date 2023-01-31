5d7ca028c40c9d48f6d16adeed9b112944c77606
pierobassa <pierobassa222@gmail.com>
2023-01-31 21:42:07 +0100

    [new] HomeHeader, NFTCard, Info about the NFT components init

---
+++ components/index.js
@@ -0,0 +1,11 @@
+import HomeHeader from "./HomeHeader";
+import NFTCard from "./NFTCard";
+import FocusedStatusBar from "./FocusedStatusBar";
+import { CircleButton } from "./Button";
+
+export {
+    HomeHeader,
+    NFTCard,
+    FocusedStatusBar,
+    CircleButton
+}
